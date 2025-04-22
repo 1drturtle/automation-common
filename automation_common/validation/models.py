076a8f77de49c50d9f47aeab38ef1d801c5a0415
Chris <1drturtle@gmail.com>
2025-04-21 21:41:35 -0400

    bump

--- automation_common/validation/models.py
+++ automation_common/validation/models.py
@@ -171,7 +171,7 @@ class PassiveEffects(AutomationModel):
     check_adv: Optional[List[AnnotatedString255]]
     check_dis: Optional[List[AnnotatedString255]]
     dc_bonus: Optional[IntExpression]
-    specific_save_bonus: Optional[List[AnnotatedString255]]
+    specific_save_bonus: Optional[Dict[str255, AnnotatedString255]]
 
 
 class AttackInteraction(AutomationModel):
